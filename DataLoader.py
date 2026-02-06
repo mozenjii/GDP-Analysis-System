525b9f43215f62b3d549adb07d99f2eb709838bf
Mozenjii <Mhbamdm@gmail.com>
2026-02-06 14:06:47 +0500

    Structure Check Done

--- DataLoader.py
+++ DataLoader.py
@@ -1,12 +1,21 @@
 import pandas as pd
 
 try:
-    ds = pd.read_excel("gdp_with_continent_filled.xlsx")
+    loadedData = pd.read_excel("gdp_with_continent_filled.xlsx")
 except:
     print("Error: ""GDP with continent data"" file not found!")
 
-if(ds.shape == (266,70)):
-    a = "000";
-else:
-    raise custom_error("Data structured in an unknown way!")
+expectedFields = {'Country Name', 'Country Code','Continent', 1960, 1961, 1962, 1963, 1964, 1965, 1966, 1967, 
+                  1968, 1969, 1970, 1971, 1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 
+                  1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 
+                  1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 
+                  2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024}
 
+if not (loadedData.shape == (266,70) and expectedFields.issubset(set(loadedData.columns))):
+    raise CustomError("Data structured in an unknown way!")
+
+ds = loadedData[list(expectedFields)].copy()
+if ds.empty:
+    raise CustomError("Restructuring Failed!")
+
+print(ds.columns)
